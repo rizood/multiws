8f22659876c0fdb4c333bb6b3a25f0331631336f
rizood <114640452+rizood@users.noreply.github.com>
2022-09-29 22:17:27 +0800

    Add files via upload

--- update/menu-speedtest.sh
+++ update/menu-speedtest.sh
@@ -78,7 +78,7 @@ speedtest
 echo -e ""
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}                • AutoScript By Reyz-V4 •          $COLOR1 $NC"
+echo -e "$COLOR1│${NC}                • Kenn XV x Alexxa •          $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e ""
 read -n 1 -s -r -p "  Press any key to back on menu"
