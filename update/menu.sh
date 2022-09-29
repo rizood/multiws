37303e63f09134b7b8e1c2b05cf2c558a42fc098
rizood <114640452+rizood@users.noreply.github.com>
2022-09-29 19:38:25 +0800

    Add files via upload

--- update/menu.sh
+++ update/menu.sh
@@ -144,7 +144,7 @@ echo "$version_up" > /opt/.ver
 echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Successfully Up To Date!"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}              • Mode By Reyz-V4 •                $COLOR1│$NC"
+echo -e "$COLOR1│${NC}              • Kenn XV x Alexxa •                $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo ""
 read -n 1 -s -r -p "  Press any key to back on menu"
@@ -227,7 +227,7 @@ else
 fi;
 echo -e "$COLOR1└─────────────────────────────────────────────────┘$NC"
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}              • Mode By Reyz-V4 •            $COLOR1│$NC"
+echo -e "$COLOR1│${NC}              • Kenn XV x Alexxa •            $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo -e ""
 echo -ne " Select menu : "; read opt
