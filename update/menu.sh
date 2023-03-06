ddcaeca09ce6cc40092f5e980e000e59f08ca4c8
rizood <114640452+rizood@users.noreply.github.com>
2023-03-07 03:36:52 +0800

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -234,7 +234,7 @@ echo -e "$COLOR1│$NC Version     :${COLOR1} $(cat /opt/.ver) Latest Version${N
 echo -e "$COLOR1│$NC Client Name : $Name"
 if [ $exp \> 1000 ];
 then
-    echo -e "$COLOR1│$NC License     : Lifetime"
+    echo -e "$COLOR1│$NC License     : $DATE"
 else
     datediff "$Exp" "$DATE"
 fi;
